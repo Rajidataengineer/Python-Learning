48dfce0dd731cc395d063e288d77862e3db4e1a9
Rajeshwari V <iamrajeshwarivenki@gmail.com>
2026-06-17 11:21:14 +0530

    Add files via upload

---
+++ String/String_Printing_Techniques.py
@@ -0,0 +1,34 @@
+#String Printing Techniques in Python
+
+# 1. Basic Print
+print('Hello,Raji!')
+
+# 2. Print with Variable
+name = 'Rajeshwari'
+print(name)
+
+# 3. String Concatenation
+first_name = 'Rajeshwari'
+last_name = 'Venkatesan'
+print(first_name + " " + last_name)
+
+# 4. f-String
+
+name = 'Rajeshwari'
+age = 25
+print(f'My name is {name} and I am {age} years old.')
+
+# 5. .format() Method
+print('my name is {} and I am {} years old.'.format(name,age))
+
+# 6. %Formatting
+print('My Name is %s and I am %d years ols.' %(name,age))
+# 7. Multi-line String
+print('''Line 1
+line 2
+Line 3''')
+
+# 8. Print with sep & end
+print('Python','SQL','ETL',sep=' | ',end=' --> Done')
+
+      
