7471efa72ffb8859f2d7949212d412c091a209b7
Rajeshwari V <iamrajeshwarivenki@gmail.com>
2026-06-18 20:19:37 +0530

    Add files via upload

---
+++ String/Python_Indexing.py
@@ -0,0 +1,29 @@
+# Python Positive & Negative Indexing
+# Python uses zero-based Indexing
+fruits = ['apple','banana','cherry','plum','grapes']
+print(fruits)
+print('-' * 50)
+print("Positive Indexing (left->right, starts at 0)")
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
+print('-' * 40)
+
+print("Negative Indexing (right->left,starts at -1)")
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+print(fruits[-4])
+print(fruits[-5])
+print('-' * 40)
+
+# How indexing on string
+name = 'Rajeshwari'
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1])
+print(name[0],name[1],name[2],name[3],name[4],name[5],name[6],name[7],name[8],name[9])
